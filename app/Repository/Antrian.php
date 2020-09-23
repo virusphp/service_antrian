cf02f1080b2569df76055bf0b1430cd09f3b6313
Slamet Sugandi <packercyber@gmail.com>
2020-09-23 14:13:12 +0700

    update refactor registrasi

--- app/Repository/Antrian.php
+++ app/Repository/Antrian.php
@@ -12,20 +12,59 @@ class Antrian
 
     public function postAntrian($params)
     {
-        $dataPasien = $this->getDataPasien($params->nomorkartu);
-        $dataPoli = $this->getDataPoli($params->kodepoli); // blum di kasih handle null
+        try {
+            $simpan = $this->simpanRegistrasi($params);
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
 
-        $dataPasien->no_telp = $params->notelp;
-        $dataPasien->nik = $params->nik;
-        $dataPasien->kd_sub_unit = $dataPoli->kd_sub_unit;
-        $dataPasien->tgl_reg = $params->tanggalperiksa;
-        $dataPasien->waktu = date('H:i:s');
-        $dataPasien->kd_cara_bayar = "8";
-        $dataPasien->no_rujukan =  $params->jenisreferensi;
-        $dataPasien->jenis_rujukan =  $params->jenisreferensi;
-        $dataPasien->polieksekutif =  $params->polieksekutif;
+    }
+
+    private function simpanRegistrasi($params)
+    {
+        // --- TABEL REGISTRASI
+        $dataPasien = $this->getDataPasien($params->nomorkartu);
+        $dataPasien->waktu          = date('H:i:s');
         dd($dataPasien);
-       
+
+        return DB::connection($this->dbsimrs)->table('registrasi')->insert([
+            // 'no_reg' => $noReg,
+            // 'no_RM' => $dataPasien->no_rm,
+            // 'tgl_reg' => $params->tanggalperiksa,,
+            // 'waktu' => $dataPasien->waktu,
+            // 'kd_asal_pasien' => $asalPasien,
+            // 'status_pengunjung' => $register['status_pengunjung'],
+            // 'kd_cara_bayar' => $register['kd_cara_bayar'],
+            // 'jenis_pasien' => $jenis_pasien,
+            // 'no_reg_pembayar' => $noReg,
+            // 'kd_penjamin' => $kd_penjamin,
+            // 'no_SJP' => '-',
+            // 'user_id' => '000'
+        ]);
+
+        // $data->tgl_reg           = $data->tanggalperiksa;
+        // $data->waktu             = date('H: i: s');
+        // $data->status_pengunjung = 1;
+        // $data->kd_cara_bayar     = 8;
+        // $data->jenis_pasien      = 0;
+        // $data->kd_penjamin       = "Cari By BPJS NO KARTU";
+        // $data->no_sjp            = "-";
+        // $data->user_id           = "0000000";
+
+        // // ---- TABEL RAWAT JALAN
+        // $data->kd_sub_unit = $dataPoli->kd_sub_unit;
+        // $data->kd_cara_kunjungan = 1;
+        // $data->status_kunjungan  = 1;
+        // $data->waktu_anamnesa    = date('H:i:s');
+        // $data->kd_dokter  = "KODE DOKTER"; 
+        // $data->reg_sms  = 3;
+
+        // $data->no_rujukan =  $params->jenisreferensi;
+        // $data->jenis_rujukan =  $params->jenisreferensi;
+        // $data->poli_eksekutif =  $params->polieksekutif;
+        // $data->nik = $params->nik;
+        // $data->no_telp = $params->notelp;
+        // dd($data);
     }
 
     private function getDataPasien($nomorKartu)
@@ -37,6 +76,12 @@ class Antrian
     private function getDataPoli($kodePoli)
     {
         return DB::connection($this->dbsimrs)->table('sub_unit')->select('kd_sub_unit')
-                ->where('kd_poli_dpjp')->first();
+                ->where('kd_poli_dpjp', $kodePoli)->first();
+    }
+
+    private function getStatusPengunjung($noRm)
+    {
+        return DB::connection($this->dbsimrs)->table('registrasi')->select('no_rm')
+                ->where('no_rm', $noRm)->get();
     }
 }

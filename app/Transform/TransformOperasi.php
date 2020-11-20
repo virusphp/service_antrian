8f0a6801726a1cd18523365bec27ffb5c75e076c
Slamet Sugandi <packercyber@gmail.com>
2020-11-20 09:41:49 +0700

    update time stamp to normal

--- app/Transform/TransformOperasi.php
+++ app/Transform/TransformOperasi.php
@@ -31,7 +31,7 @@ class TransformOperasi
                     'namapoli'       => $value->nama,
                     'terlaksana'     => (int)$value->status_proses,
                     'nopeserta'      => $value->no_kartu,
-                    'lastupdate'     => lastUpdate($value->tgl_tindakan)
+                    'lastupdate'     => time() * 1000
             ];
         }
 

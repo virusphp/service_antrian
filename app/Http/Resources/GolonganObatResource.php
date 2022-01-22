41950414cc7a3eec061570bf028d589a4909cd3b
virusphp <packercyber@gmail.com>
2022-01-22 09:34:43 +0700

    fix ambigu

--- app/Http/Resources/GolonganObatResource.php
+++ app/Http/Resources/GolonganObatResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class JenisObatResource extends JsonResource
+class GolonganObatResource extends JsonResource
 {
     /**
      * Transform the resource into an array.

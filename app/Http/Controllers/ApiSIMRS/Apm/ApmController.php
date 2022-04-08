533047c612f983c6b7ff343053cef800605947da
nexterid <nexter.three@gmail.com>
2022-04-08 09:51:24 +0700

    update service apm

--- app/Http/Controllers/ApiSIMRS/Apm/ApmController.php
+++ app/Http/Controllers/ApiSIMRS/Apm/ApmController.php
@@ -12,14 +12,12 @@ class ApmController extends Controller
     protected $registrasi;
     protected $antrian;
     protected $transform;
-    protected $serviceBpjs;
 
     public function __construct()
     {
         $this->registrasi = new Registrasi;
         $this->antrian = new Antrian;
         $this->transform = new TransformRegistrasi;
-        $this->serviceBpjs = new AppSep;
     }
 
     public function dataRegistrasi($noRm)
@@ -51,36 +49,13 @@ class ApmController extends Controller
         return response()->jsonApi("200", "OK", $transform);
     }
 
+    public function jumlahSep($jnsRujukan, $noRujukan)
+    {
+        //
+    }
+
     public function insertSep(Request $request)
     {
-        $dataSep = $request->all();
-        $result = $this->serviceBpjs->insertSep($dataSep);
-        $res = json_decode($result);
-        if ($res->metaData->code == 200) {
-            if ($res->response->sep->peserta->noKartu == $params['no_kartu'] && $res->response->sep->peserta->noMr == $params['no_rm']) {
-                DB::beginTransaction();
-                try {
-                    $dataInsert = $this->handleRequestData($params, $res->response->sep->noSep);
-                    // $dataInsert = $this->handleRequestData($params);
-                    $this->simpanSep($dataInsert);
-                    // dd($insert);
-                    $this->simpanRujukan($params, $dataInsert);
-
-                    // $this->serviceTask->sendTask(json_encode($dataInsert["task"]));
-                    DB::commit();
-                    return $result;
-                } catch (\Illuminate\Database\QueryException $e) {
-                    DB::rollback();
-                    if ($e->getCode() == "23000") {
-                        return $result;
-                    }
-                }
-            } else {
-                $message = $this->getMessage($result);
-                return $message;
-            }
-        } else {
-            return $result;
-        }
+        //
     }
 }
